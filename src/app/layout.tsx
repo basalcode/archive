2720c9e146e0e6d97ef64d975525b1d2395e0e97
basalcode <basalcode@gmail.com>
2025-06-28 01:33:40 +0900

    Archive 프로젝트 시작 !

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Archive",
+  description: "Hello, to the archive.",
+};
+
+export interface Props {
+  children: React.ReactNode;
+};
+
+const RootLayout = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <main>
+      <h1>Hello, to the archive.</h1>
+    </main>
+  );
+}
+
+export default Page;
